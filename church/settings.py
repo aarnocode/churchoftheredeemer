36a97e81be96f7be5cf62f307341c1da3073353c
aarnocode <jamesaaron.miranda@gmail.com>
2020-07-10 19:28:43 +0800

    add dj_urls and some stuff

--- church/settings.py
+++ church/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-
+import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR=os.path.join(BASE_DIR,'templates')
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -122,3 +123,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS=[
     STATIC_DIR,
 ]
+STATICFILES_STORAGE='whitenoise.django.GzipManifestStaticFilesStorage'
+prod_db=dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
