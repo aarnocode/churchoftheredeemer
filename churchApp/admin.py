22d7ffed87dc95a6e96fabace237da8e8b7ff76d
aarnocode <jamesaaron.miranda@gmail.com>
2020-07-15 01:19:24 +0800

    Changed site header to Church of the REdeemer

--- churchApp/admin.py
+++ churchApp/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
+from django.contrib.admin import AdminSite
 from churchApp.models import DevotionalVerse,VerseOfTheDay,Sermon,Announcement
+
 # Register your models here.
 admin.site.register(VerseOfTheDay)
 admin.site.register(DevotionalVerse)
 admin.site.register(Sermon)
 admin.site.register(Announcement)
+admin.site.site_header='Church of the Redeemer'
